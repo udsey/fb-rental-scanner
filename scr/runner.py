f295c1a6bcd42d33c8888cd649ea5982f165b7c1
udsey <yuuhik@yandex.ru>
2026-05-02 23:55:51 +0700

    fix log position

--- scr/runner.py
+++ scr/runner.py
@@ -75,9 +75,8 @@ if __name__ == "__main__":
                 if decision:
                     review_apartments()
                     generate_messages()
-                    
+            logger.info(f"Done. Next run scheduled in {config.system_config.runner_config.interval_mins} minutes")             
             time.sleep(config.system_config.runner_config.interval_mins * 60)
-            logger.info(f"Done. Next run scheduled in {config.system_config.runner_config.interval_mins} minutes")
         except Exception as e:
             logger.debug(e, exc_info=True)
 
